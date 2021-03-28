15496148986b5bb585837fc43519cf924e66d794
17520720 <50265171+17520720@users.noreply.github.com>
2021-03-28 19:43:23 +0700

    Update CauHoi.js
    Store

--- tracnghiem-client/src/views/Admin/CauHoi.js
+++ tracnghiem-client/src/views/Admin/CauHoi.js
@@ -3,6 +3,7 @@ import { Component } from "react";
 export default class CauHoiPage extends Component {
   constructor(props) {
     super(props);
+
     this.state = {
       txt_noidung: "",
       dapan: {
@@ -20,24 +21,41 @@ export default class CauHoiPage extends Component {
     var target = event.target;
     var name = target.name;
     var value = target.value;
+
     this.setState({
       [name]: value,
     });
   };
 
+  handleChangeAnswer = (event) => {
+
+    let value = event.target.value;
+    let name = event.target.name;
+
+    this.setState({
+      dapan: {
+        ...this.state.dapan,
+        [name]: value
+      } 
+    })
+  }
+
   handleSubmit = (event) => {
+
     event.preventDefault();
     localStorage.setItem("CauHoi", JSON.stringify(this.state));
+    
   };
 
   render() {
-    console.log(this.state);
+
     var {
       txt_noidung,
       dapan,
       select_doKho,
       select_dangToan,
     } = this.state;
+
     return (
       <div>
         <form onSubmit={this.handleSubmit}>
@@ -51,6 +69,7 @@ export default class CauHoiPage extends Component {
               value={txt_noidung}
               onChange={this.handleChange}
             />
+
             <div className="form-group">
               <label htmlFor="dapan1">Đáp án 1</label>
               <input
@@ -59,7 +78,7 @@ export default class CauHoiPage extends Component {
                 id="dapan1"
                 name="dapan1"
                 value={dapan.dapan1}
-                onChange={this.handleChange}
+                onChange={this.handleChangeAnswer}
               />
             </div>
             <div className="form-group">
@@ -70,7 +89,7 @@ export default class CauHoiPage extends Component {
                 id="dapan2"
                 name="dapan2"
                 value={dapan.dapan2}
-                onChange={this.handleChange}
+                onChange={this.handleChangeAnswer}
               />
             </div>
             <div className="form-group">
@@ -81,7 +100,7 @@ export default class CauHoiPage extends Component {
                 id="dapan3"
                 name="dapan3"
                 value={dapan.dapan3}
-                onChange={this.handleChange}
+                onChange={this.handleChangeAnswer}
               />
             </div>
             <div className="form-group">
@@ -92,12 +111,14 @@ export default class CauHoiPage extends Component {
                 id="dapan4"
                 name="dapan4"
                 value={dapan.dapan4}
-                onChange={this.handleChange}
+                onChange={this.handleChangeAnswer}
               />
             </div>
+
           </div>
           <div className="row">
             <div className="col-xs-2 col-sm-2 col-md-2 col-lg-2">
+
               <div className="form-group">
                 <label htmlFor="select_doKho">Độ khó</label>
                 <select
@@ -113,8 +134,10 @@ export default class CauHoiPage extends Component {
                   <option value={4}>Vận dụng cao</option>
                 </select>
               </div>
+              
             </div>
             <div className="col-xs-4 col-sm-4 col-md-4 col-lg-4">
+
               <div className="form-group">
                 <label htmlFor="select_dangToan">Dạng toán</label>
                 <select
@@ -130,6 +153,7 @@ export default class CauHoiPage extends Component {
                   <option value={4}>Dạng toán 4</option>
                 </select>
               </div>
+
             </div>
           </div>
 
