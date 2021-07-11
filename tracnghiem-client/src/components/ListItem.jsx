517a597356b9b79a56392a26b2cc2ef8c6ac0e10
17520720 <50265171+17520720@users.noreply.github.com>
2021-07-11 19:02:09 +0700

    admin submit

--- tracnghiem-client/src/components/ListItem.jsx
+++ tracnghiem-client/src/components/ListItem.jsx
@@ -1,8 +1,11 @@
 import { MathJax, MathJaxContext } from "better-react-mathjax";
 import React from "react";
+import { useState } from "react";
 import { mathjax_config } from "../constants/config";
 
 const ListItemRoot = ({ item }) => {
+     const [displayAns, setDisplayAns] = useState(false);
+
      const classLevel = () => {
           if (item?.level == 3) {
                return `#fc7373`;
@@ -28,27 +31,62 @@ const ListItemRoot = ({ item }) => {
           }
      };
 
+     const styleList = () => {
+          if (!displayAns) {
+               return {
+                    height: "0px",
+                    overflow: "hidden",
+                    marginTop: "0px",
+               }
+          }
+          else {
+               return {}
+          }
+     }
+
+     function toggleShowAns() {
+          setDisplayAns(!displayAns);
+     }
+
      return (
-          <div className="list-item">
-               <MathJaxContext version={3} config={mathjax_config}>
-                    <div className="list-item__name">
-                         <strong>#{item?._id}</strong> <br />
-                         <MathJax>{item?.content}</MathJax>
-                    </div>
-                    <div className="list-item__topic">{item?.topic?.content}</div>
-                    <div className="list-item__level" style={{ color: classLevel() }}>
-                         {Difficult(item?.level)}
-                    </div>
-                    <div className="option-container">
-                         <div className="correct-rate">{
-                              isNaN(item?.correctAns / item?.countAns)
-                              ? 0
-                              : (item.correctAns / item.countAns) }</div>
-                         <div className="edit-option">Edit</div>
-                         <div className="delete-option">Delete</div>
-                    </div>
-               </MathJaxContext>
-          </div>
+          <>
+               <div className="list-item" onClick={toggleShowAns}>
+                    <MathJaxContext version={3} config={mathjax_config}>
+                         <div className="question-body">
+                              <div className="list-item__name">
+                                   <strong>#{item?._id}</strong> <br />
+                                   <MathJax>{item?.content}</MathJax>
+                              </div>
+                              <div className="list-item__topic">{item?.topic?.content}</div>
+                              <div className="list-item__level" style={{ color: classLevel() }}>
+                                   {Difficult(item?.level)}
+                              </div>
+                              <div className="option-container">
+                                   <div className="correct-rate">{
+                                        isNaN(item?.correctAns / item?.countAns)
+                                        ? 0
+                                        : (item.correctAns / item.countAns) }</div>
+                                   <div className="edit-option">Edit</div>
+                                   <div className="delete-option">Delete</div>
+                              </div>
+                         </div>
+                         <div className="set-questions" style={styleList()}>
+                              {
+                                   item?.setOfAnswer.map(answer => {
+                                        return (
+                                             <div style={{ marginBottom: "0.5rem", 
+                                                           fontWeight: answer.isCorrect? "500" : "unset"
+                                                       }}
+                                                  key={answer.index}>
+                                                  <MathJax>{answer.content}</MathJax>
+                                             </div>
+                                        )
+                                   })
+                              }
+                         </div>
+                    </MathJaxContext>
+               </div>
+          </>
      );
 };
 
